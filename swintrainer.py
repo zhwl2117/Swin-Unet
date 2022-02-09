224ad03420ded5b5f4e042409a59b07e3ca38515
zhwl2117 <1799878714@qq.com>
2022-02-09 08:53:10 -0800

    add swin trainer

--- swintrainer.py
+++ swintrainer.py
@@ -32,6 +32,7 @@ from loss_fn.dice_loss import DC_and_CE_loss
 from loss_fn.deep_supervision import MultipleOutputLoss2
 from datasets.utils.dataset_loading import load_dataset, unpack_dataset
 from datasets.datagen_mdi import default_3D_augmentation_params, default_2D_augmentation_params, get_patch_size, get_moreDA_augmentation
+from swinutils import InitWeights_He, poly_lr, to_cuda, maybe_to_torch
 
 
 class SwinTrainer(object):
@@ -489,3 +490,384 @@ class SwinTrainer(object):
                 ctr += 1
         if also_print_to_console:
             print(*args)
+
+    def run_training(self):
+        """
+        if we run with -c then we need to set the correct lr for the first epoch, otherwise it will run the first
+        continued epoch with self.initial_lr
+
+        we also need to make sure deep supervision in the network is enabled for training, thus the wrapper
+        :return:
+        """
+        # if we dont overwrite epoch then self.epoch+1 is used which is not what we
+        # want at the start of the training
+        self.maybe_update_lr(self.epoch)
+
+        #TODO this function maynot be applicable at present
+        ds = self.network.do_ds 
+        self.network.do_ds = True
+
+        #  here from nnUNetTrainerV1
+        self.save_debug_information()
+
+        # here from nnUNet generic traniner
+        self._run_training()
+
+    def _run_training(self):
+        if not torch.cuda.is_available():
+            self.print_to_log_file("WARNING!!! You are attempting to run training on a CPU (torch.cuda.is_available() is False). This can be VERY slow!")
+
+        _ = self.tr_gen.next()
+        _ = self.val_gen.next()
+
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+
+        #TODO check the code here
+        self._maybe_init_amp()
+
+        maybe_mkdir_p(self.output_folder)
+        self.plot_network_architecture()
+
+        if cudnn.benchmark and cudnn.deterministic:
+            warn("torch.backends.cudnn.deterministic is True indicating a deterministic training is desired. "
+                "But torch.backends.cudnn.benchmark is True as well and this will prevent deterministic training! "
+                "If you want deterministic then set benchmark=False")
+
+        if not self.was_initialized:
+            self.initialize(True)
+
+        while self.epoch < self.max_num_epochs:
+            self.print_to_log_file("\nepoch: ", self.epoch)
+            epoch_start_time = time()
+            train_losses_epoch = []
+
+            self.network.train()
+
+            if self.use_progress_bar:
+                with trange(self.num_batches_per_epoch) as tbar:
+                    for b in tbar:
+                        tbar.set_description("Epoch {}/{}".format(self.epoch+1, self.max_num_epochs))
+                        l = self.run_iteration(self.tr_gen, True)
+
+                        tbar.set_postfix(loss=l)
+                        train_losses_epoch.append(l)
+            else:
+                for _ in range(self.num_batches_per_epoch):
+                    l = self.run_iteration(self.tr_gen, True)
+                    train_losses_epoch.append(l)
+
+            self.all_tr_losses.append(np.mean(train_losses_epoch))
+            self.print_to_log_file("train loss : %.4f" % self.all_tr_losses[-1])
+
+            with torch.no_grad():
+                self.network.eval()
+                val_losses = []
+                for b in range(self.num_val_batches_per_epoch):
+                    l = self.run_iteration(self.val_gen, False, True)
+                    val_losses.append(l)
+                self.all_val_losses.append(np.mean(val_losses))
+                self.print_to_log_file("validation loss: %.4f" % self.all_val_losses[-1])
+
+                if self.also_val_in_tr_mode:
+                    self.network.train()
+                    val_losses = []
+                    for b in range(self.num_val_batches_per_epoch):
+                        l = self.run_iteration(self.val_gen, False)
+                        val_losses.append(l)
+                    self.all_val_losses_tr_mode.append(np.mean(val_losses))
+                    self.print_to_log_file("validation loss (train=True): %.4f" % self.all_val_losses_tr_mode[-1])
+
+            self.update_train_loss_MA()  # needed for lr scheduler and stopping of training
+
+            continue_training = self.on_epoch_end()
+
+            epoch_end_time = time()
+
+            if not continue_training:
+                # early stop
+                break
+
+            self.epoch += 1
+            self.print_to_log_file("This epoch took %f s\n" % (epoch_end_time - epoch_start_time))
+
+        self.epoch -= 1
+
+        if self.save_final_checkpoint:
+            self.save_checkpoint(join(self.output_folder, "model_final_checkpoint.model"))
+        # now we can delete latest as it will be identical with final
+        if isfile(join(self.output_folder, "model_latest.model")):
+            os.remove(join(self.output_folder, "model_latest.model"))
+        if isfile(join(self.output_folder, "model_latest.model.pkl")):
+            os.remove(join(self.output_folder, "model_latest.model.pkl"))
+
+    def save_checkpoint(self, fname, save_optimizer=True):
+        start_time = time()
+        state_dict = self.network.state_dict()
+        for key in state_dict.keys():
+            state_dict[key] = state_dict[key].cpu()
+        lr_sched_state_dct = None
+        if self.lr_scheduler is not None and hasattr(self.lr_scheduler,
+                                                    'state_dict'):  # not isinstance(self.lr_scheduler, lr_scheduler.ReduceLROnPlateau):
+            lr_sched_state_dct = self.lr_scheduler.state_dict()
+            # WTF is this!?
+            # for key in lr_sched_state_dct.keys():
+            #    lr_sched_state_dct[key] = lr_sched_state_dct[key]
+        if save_optimizer:
+            optimizer_state_dict = self.optimizer.state_dict()
+        else:
+            optimizer_state_dict = None
+
+        self.print_to_log_file("saving checkpoint...")
+        save_this = {
+            'epoch': self.epoch + 1,
+            'state_dict': state_dict,
+            'optimizer_state_dict': optimizer_state_dict,
+            'lr_scheduler_state_dict': lr_sched_state_dct,
+            'plot_stuff': (self.all_tr_losses, self.all_val_losses, self.all_val_losses_tr_mode,
+                        self.all_val_eval_metrics),
+            'best_stuff' : (self.best_epoch_based_on_MA_tr_loss, self.best_MA_tr_loss_for_patience, self.best_val_eval_criterion_MA)}
+        if self.amp_grad_scaler is not None:
+            save_this['amp_grad_scaler'] = self.amp_grad_scaler.state_dict()
+
+        torch.save(save_this, fname)
+        self.print_to_log_file("done, saving took %.2f seconds" % (time() - start_time))
+
+    def on_epoch_end(self):
+        self.finish_online_evaluation()  
+        # does not have to do anything, but can be used to update self.all_val_eval_metrics
+
+        self.plot_progress()
+        self.maybe_update_lr()
+        self.maybe_save_checkpoint()
+        self.update_train_loss_MA()
+
+        continue_training = self.epoch < self.max_num_epochs
+        # it can rarely happen that the momentum of nnUNetTrainerV2 is too high for some dataset. If at epoch 100 the
+        # estimated validation Dice is still 0 then we reduce the momentum from 0.99 to 0.95
+        if self.epoch == 100:
+            if self.all_val_eval_metrics[-1] == 0:
+                self.optimizer.param_groups[0]["momentum"] = 0.95
+                self.network.apply(InitWeights_He(1e-2))
+                self.print_to_log_file("At epoch 100, the mean foreground Dice was 0. This can be caused by a too "
+                                    "high momentum. High momentum (0.99) is good for datasets where it works, but "
+                                    "sometimes causes issues such as this one. Momentum has now been reduced to "
+                                    "0.95 and network weights have been reinitialized")
+        return continue_training
+
+    def manage_patience(self):
+        # update patience
+        continue_training = True
+        if self.patience is not None:
+            # if best_MA_tr_loss_for_patience and best_epoch_based_on_MA_tr_loss were not yet initialized,
+            # initialize them
+            if self.best_MA_tr_loss_for_patience is None:
+                self.best_MA_tr_loss_for_patience = self.train_loss_MA
+
+            if self.best_epoch_based_on_MA_tr_loss is None:
+                self.best_epoch_based_on_MA_tr_loss = self.epoch
+
+            if self.best_val_eval_criterion_MA is None:
+                self.best_val_eval_criterion_MA = self.val_eval_criterion_MA
+
+            # check if the current epoch is the best one according to moving average of validation criterion. If so
+            # then save 'best' model
+            # Do not use this for validation. This is intended for test set prediction only.
+            #self.print_to_log_file("current best_val_eval_criterion_MA is %.4f0" % self.best_val_eval_criterion_MA)
+            #self.print_to_log_file("current val_eval_criterion_MA is %.4f" % self.val_eval_criterion_MA)
+
+            if self.val_eval_criterion_MA > self.best_val_eval_criterion_MA:
+                self.best_val_eval_criterion_MA = self.val_eval_criterion_MA
+                #self.print_to_log_file("saving best epoch checkpoint...")
+                if self.save_best_checkpoint: self.save_checkpoint(join(self.output_folder, "model_best.model"))
+
+            # Now see if the moving average of the train loss has improved. If yes then reset patience, else
+            # increase patience
+            if self.train_loss_MA + self.train_loss_MA_eps < self.best_MA_tr_loss_for_patience:
+                self.best_MA_tr_loss_for_patience = self.train_loss_MA
+                self.best_epoch_based_on_MA_tr_loss = self.epoch
+                #self.print_to_log_file("New best epoch (train loss MA): %03.4f" % self.best_MA_tr_loss_for_patience)
+            else:
+                pass
+                #self.print_to_log_file("No improvement: current train MA %03.4f, best: %03.4f, eps is %03.4f" %
+                #                       (self.train_loss_MA, self.best_MA_tr_loss_for_patience, self.train_loss_MA_eps))
+
+            # if patience has reached its maximum then finish training (provided lr is low enough)
+            if self.epoch - self.best_epoch_based_on_MA_tr_loss > self.patience:
+                if self.optimizer.param_groups[0]['lr'] > self.lr_threshold:
+                    #self.print_to_log_file("My patience ended, but I believe I need more time (lr > 1e-6)")
+                    self.best_epoch_based_on_MA_tr_loss = self.epoch - self.patience // 2
+                else:
+                    #self.print_to_log_file("My patience ended")
+                    continue_training = False
+            else:
+                pass
+                #self.print_to_log_file(
+                #    "Patience: %d/%d" % (self.epoch - self.best_epoch_based_on_MA_tr_loss, self.patience))
+
+        return continue_training
+
+    def update_train_loss_MA(self):
+        if self.train_loss_MA is None:
+            self.train_loss_MA = self.all_tr_losses[-1]
+        else:
+            self.train_loss_MA = self.train_loss_MA_alpha * self.train_loss_MA + (1 - self.train_loss_MA_alpha) * \
+                                self.all_tr_losses[-1]
+
+    def maybe_save_checkpoint(self):
+        """
+        Saves a checkpoint every save_ever epochs.
+        :return:
+        """
+        if self.save_intermediate_checkpoints and (self.epoch % self.save_every == (self.save_every - 1)):
+            self.print_to_log_file("saving scheduled checkpoint file...")
+            if not self.save_latest_only:
+                self.save_checkpoint(join(self.output_folder, "model_ep_%03.0d.model" % (self.epoch + 1)))
+            self.save_checkpoint(join(self.output_folder, "model_latest.model"))
+            self.print_to_log_file("done")
+
+    def finish_online_evaluation(self):
+        self.online_eval_tp = np.sum(self.online_eval_tp, 0)
+        self.online_eval_fp = np.sum(self.online_eval_fp, 0)
+        self.online_eval_fn = np.sum(self.online_eval_fn, 0)
+
+        global_dc_per_class = [i for i in [2 * i / (2 * i + j + k) for i, j, k in
+                                        zip(self.online_eval_tp, self.online_eval_fp, self.online_eval_fn)]
+                            if not np.isnan(i)]
+        self.all_val_eval_metrics.append(np.mean(global_dc_per_class))
+
+        self.print_to_log_file("Average global foreground Dice:", [np.round(i, 4) for i in global_dc_per_class])
+        self.print_to_log_file("(interpret this as an estimate for the Dice of the different classes. This is not "
+                            "exact.)")
+
+        self.online_eval_foreground_dc = []
+        self.online_eval_tp = []
+        self.online_eval_fp = []
+        self.online_eval_fn = []
+
+    def plot_progress(self):
+        """
+        Should probably by improved
+        :return:
+        """
+        try:
+            font = {'weight': 'normal',
+                    'size': 18}
+
+            matplotlib.rc('font', **font)
+
+            fig = plt.figure(figsize=(30, 24))
+            ax = fig.add_subplot(111)
+            ax2 = ax.twinx()
+
+            x_values = list(range(self.epoch + 1))
+
+            ax.plot(x_values, self.all_tr_losses, color='b', ls='-', label="loss_tr")
+
+            ax.plot(x_values, self.all_val_losses, color='r', ls='-', label="loss_val, train=False")
+
+            if len(self.all_val_losses_tr_mode) > 0:
+                ax.plot(x_values, self.all_val_losses_tr_mode, color='g', ls='-', label="loss_val, train=True")
+            if len(self.all_val_eval_metrics) == len(x_values):
+                ax2.plot(x_values, self.all_val_eval_metrics, color='g', ls='--', label="evaluation metric")
+
+            ax.set_xlabel("epoch")
+            ax.set_ylabel("loss")
+            ax2.set_ylabel("evaluation metric")
+            ax.legend()
+            ax2.legend(loc=9)
+
+            fig.savefig(join(self.output_folder, "progress.png"))
+            plt.close()
+        except IOError:
+            self.print_to_log_file("failed to plot: ", sys.exc_info())
+
+    def run_iteration(self, data_generator, do_backprop=True, run_online_evaluation=False):
+        """
+        gradient clipping improves training stability
+
+        :param data_generator:
+        :param do_backprop:
+        :param run_online_evaluation:
+        :return:
+        """
+        data_dict = next(data_generator)
+        data = data_dict['data']
+        target = data_dict['target']
+
+        data = maybe_to_torch(data)
+        target = maybe_to_torch(target)
+
+        if torch.cuda.is_available():
+            data = to_cuda(data)
+            target = to_cuda(target)
+
+        self.optimizer.zero_grad()
+
+        if self.fp16:
+            with autocast():
+                output = self.network(data)
+                del data
+                l = self.loss(output, target)
+
+            if do_backprop:
+                self.amp_grad_scaler.scale(l).backward()
+                self.amp_grad_scaler.unscale_(self.optimizer)
+                torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
+                self.amp_grad_scaler.step(self.optimizer)
+                self.amp_grad_scaler.update()
+        else:
+            output = self.network(data)
+            del data
+            l = self.loss(output, target)
+
+            if do_backprop:
+                l.backward()
+                torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
+                self.optimizer.step()
+
+        if run_online_evaluation:
+            self.run_online_evaluation(output, target)
+
+        del target
+
+        return l.detach().cpu().numpy()
+
+    def maybe_update_lr(self, epoch=None):
+        if epoch is None:
+            ep = self.epoch + 1
+        else:
+            ep = epoch
+        self.optimizer.param_groups[0]['lr'] = poly_lr(ep, self.max_num_epochs, self.initial_lr, 0.9)
+        self.print_to_log_file("lr:", np.round(self.optimizer.param_groups[0]['lr'], decimals=6))
+
+    def plot_network_architecture(self):
+        """
+        can be implemented (see nnUNetTrainer) but does not have to. Not implemented here because it imposes stronger
+        assumptions on the presence of class variables
+        :return:
+        """
+        #TODO coordinate with the Swin UNet
+        pass
+
+    def _maybe_init_amp(self):
+        if self.fp16 and self.amp_grad_scaler is None:
+            self.amp_grad_scaler = GradScaler()
+
+    def save_debug_information(self):
+        dct = OrderedDict()
+        for k in self.__dir__():
+            if not k.startswith("__"):
+                if not callable(getattr(self, k)):
+                    dct[k] = str(getattr(self, k))
+        del dct['plans']
+        del dct['plans']
+        del dct['intensity_properties']
+        del dct['dataset']
+        del dct['dataset_tr']
+        del dct['dataset_val']
+        save_json(dct, join(self.output_folder, "debug.json"))
+
+        import shutil
+
+        shutil.copy(self.plans_file, join(self.output_folder_base, "plans.pkl"))
---
+++ swinutils.py
@@ -0,0 +1,35 @@
+#  this code is heavily borrow from the nnUNet
+#  For more details, please refer to https://github.com/MIC-DKFZ/nnUNet
+
+
+import torch
+import torch.nn as nn
+
+
+class InitWeights_He(object):
+    def __init__(self, neg_slope=1e-2):
+        self.neg_slope = neg_slope
+
+    def __call__(self, module):
+        if isinstance(module, nn.Conv3d) or isinstance(module, nn.Conv2d) or isinstance(module, nn.ConvTranspose2d) or isinstance(module, nn.ConvTranspose3d):
+            module.weight = nn.init.kaiming_normal_(module.weight, a=self.neg_slope)
+            if module.bias is not None:
+                module.bias = nn.init.constant_(module.bias, 0)
+
+def poly_lr(epoch, max_epochs, initial_lr, exponent=0.9):
+    return initial_lr * (1 - epoch / max_epochs)**exponent
+
+def maybe_to_torch(d):
+    if isinstance(d, list):
+        d = [maybe_to_torch(i) if not isinstance(i, torch.Tensor) else i for i in d]
+    elif not isinstance(d, torch.Tensor):
+        d = torch.from_numpy(d).float()
+    return d
+
+
+def to_cuda(data, non_blocking=True, gpu_id=0):
+    if isinstance(data, list):
+        data = [i.cuda(gpu_id, non_blocking=non_blocking) for i in data]
+    else:
+        data = data.cuda(gpu_id, non_blocking=non_blocking)
+    return data
